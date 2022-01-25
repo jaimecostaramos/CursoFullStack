99fd8246adbc172fc9c2f160c158bc75e5788e3e
Jaime Costa <jaimecostaramos@gmail.com>
2022-01-25 10:05:57 -0300

    Projeto Criado

---
+++ src/main/java/com/jaimecosta/cursofullstack/CursofullstackApplication.java
@@ -0,0 +1,13 @@
+package com.jaimecosta.cursofullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursofullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursofullstackApplication.class, args);
+	}
+
+}
