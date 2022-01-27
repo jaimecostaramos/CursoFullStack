7e862e7e8e02dec3831f24480e5c5d551e99f155
Jaime Costa <jaimecostaramos@gmail.com>
2022-01-27 20:58:23 -0300

    Criando operacao de instanciacao

--- src/main/java/com/jaimecosta/cursofullstack/CursofullstackApplication.java
+++ src/main/java/com/jaimecosta/cursofullstack/CursofullstackApplication.java
@@ -1,13 +1,35 @@
 package com.jaimecosta.cursofullstack;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursofullstackApplication {
+import com.jaimecosta.cursofullstack.domain.Categoria;
+import com.jaimecosta.cursofullstack.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursofullstackApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriarepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursofullstackApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria (null, "Informática");
+		Categoria cat2 = new Categoria (null, "Escritório");
+		
+		categoriarepository.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
+	
+	
 
 }
